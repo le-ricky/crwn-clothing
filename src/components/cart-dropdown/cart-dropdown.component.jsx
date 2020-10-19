4de52500504992343f86ed3b6b301c237dae56ea
Ricky Le <Ricky.Le88@gmail.com>
2020-10-19 18:01:15 -0400

    created cart-item component and connect cart-dropdown with cartReducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,12 +2,23 @@ import React from 'react';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CustomButtom from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items' />
+        {
+            cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+            ))
+        }
         <CustomButtom>GO TO CHECKOUT</CustomButtom>
     </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems }}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
